d4ea473eaeb7c0cf86acb7e6a3c3473fa7911287
Sleeping_A <73869314+ALLVIS-II@users.noreply.github.com>
2023-03-23 11:05:26 +1030

    Create week5.py

---
+++ week5.py
@@ -0,0 +1,32 @@
+#git@github.com:ALLVIS-II/wek-5-.git
+
+#Question 1 
+#a)
+#0,1,2,3,
+
+#b)
+#0,2,4,6,8
+
+#c)
+#Sooo much fun
+
+#d)
+#0,2,4,6,8
+
+#e)
+#oomc u
+
+#f)
+#yes it is 
+
+#g)
+#WTF!?!
+
+
+#question 3 
+list1 = [7, 21, 11, 2, 5, 1, 15]
+
+list2 = [0, 0, 0, 0, 0, 0, 0]
+
+myList = list1(1) + list2(3)
+print(myList)
