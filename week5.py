00816c4844d601ad13c5c7d334fb0dc57391ada3
Sleeping_A <73869314+ALLVIS-II@users.noreply.github.com>
2023-03-23 12:36:26 +1030

    Update week5.py

--- week5.py
+++ week5.py
@@ -1,5 +1,3 @@
-#git@github.com:ALLVIS-II/wek-5-.git
-
 #Question 1 
 #a)
 #0,1,2,3,
@@ -22,7 +20,11 @@
 #g)
 #WTF!?!
 
+#h) 
+#FTW!?!
 
+#i)
+#3, 8, 2, 7, 1 
 #question 3 
 list1 = [7, 21, 11, 2, 5, 1, 15]
 
@@ -30,3 +32,25 @@ list2 = [0, 0, 0, 0, 0, 0, 0]
 
 myList = list1(1) + list2(3)
 print(myList)
+
+
+#week4
+#question 8
+
+sing = input("would you like to sing a song(y/n)?" )
+
+if sing == "y";
+count = 0 
+num_verses = int(input("How many verses of the song do you wish to sing")
+                 num_bottles = 100
+                 while count < num_verses:
+                 print(num_bottles, "bottles of beer on the wall")
+                 print(num_botles, "bottles of beer")
+                 print ("if one of those bottles should happen to fall")
+                 print(num_bottles-1, " bottles of beer on the wall\n")
+                 
+                 num_bottles = num_bottles - 1 
+                 count = ount + 1
+                 print("That was fun! Would you like to sing again (y/n)")
+                 print("Thanks for singing along!")
+                      
